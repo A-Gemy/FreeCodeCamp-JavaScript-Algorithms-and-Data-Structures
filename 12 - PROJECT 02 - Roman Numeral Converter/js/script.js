6e628a64a777b6f495387b961a1d24e6e5d06611
Ahmed Gamal <ahmgy.mail@gmail.com>
2024-05-19 19:49:06 +0300

    update roman numeral converter functions for better maintenance and readability

--- 12 - PROJECT 02 - Roman Numeral Converter/js/script.js
+++ 12 - PROJECT 02 - Roman Numeral Converter/js/script.js
@@ -2,28 +2,39 @@ const convertBtn = document.getElementById("convert-btn");
 const inputNumber = document.getElementById("number");
 const output = document.getElementById("output");
 
+// The validateInputValue function checks if the input is a valid number and within a specific range.
+function validateInputValue(inputValue) {
+  if (!inputValue || Number.isNaN(inputValue)) {
+    return "Please enter a valid number";
+  } else if (inputValue <= 0) {
+    return "Please enter a number greater than or equal to 1";
+  } else if (inputValue > 3999) {
+    return "Please enter a number less than or equal to 3999";
+  }
+  return null;
+}
+
+// The checkInputValue function retrieves the input value, validates it, and updates the output accordingly.
 function checkInputValue() {
-  if (!inputNumber.value) {
+  const inputValue = Number(inputNumber.value);
+  const validationMessage = validateInputValue(inputValue);
+
+  if (validationMessage) {
     updateAnError();
-    output.textContent = "Please enter a valid number";
-  } else if (inputNumber.value <= 0) {
-    updateAnError();
-    output.textContent = "Please enter a number greater than or equal to 1";
-  } else if (inputNumber.value > 3999) {
-    updateAnError();
-    output.textContent = "Please enter a number less than or equal to 3999";
+    output.textContent = validationMessage;
   } else {
-    output.classList.remove("error");
-    output.classList.remove("hidden");
-    output.textContent = convertToRoman(inputNumber.value);
+    output.classList.remove("error", "hidden");
+    output.textContent = convertToRoman(inputValue);
   }
 }
 
+// If the input is invalid, this function display an error message.
 function updateAnError() {
   output.classList.remove("hidden");
   output.classList.add("error");
 }
 
+// The convertToRoman function converts the input number to a Roman numeral using a mapping object and a repeat method for efficiency.
 function convertToRoman(num) {
   /* THIS FUNCTION USES MAPPING FUNCTION APPROACH. */
   // Create a mapping object where keys are Roman numerals and values are their corresponding integer values
